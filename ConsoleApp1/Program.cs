3800e5b7e3602c5b577ee8573c800b4e3633aa25
FarrukhSadikhov <farrukh.sadikhov@outlook.com>
2021-12-12 02:53:52 +0400

    exercise 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("стоимости поездки на такси в манатах, в зависимости от расстояния");
+            Console.Write("Введите расстояния:");
+            // 1 км стоит 1,5$
+            // f - доллар равен курсу по ЦБ (на сегодняшний день 1$ - 1,7 манат)
+
+            var f = 2.55;
+            int x = Convert.ToInt16(Console.ReadLine());
+
+            double rasstoyaniye  = x * f;
+
+            Console.WriteLine($"расстоляние: {rasstoyaniye} манат");
+            Console.ReadKey();
+
+
+
+       
+        }
+    }
+}
