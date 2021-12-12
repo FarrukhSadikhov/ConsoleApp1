c2c5cf8a39b6c58d6425f4ce2655dbb78eb77cac
FarrukhSadikhov <farrukh.sadikhov@outlook.com>
2021-12-12 21:15:18 +0400

    no comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("стоимости поездки на такси в манатах, в зависимости от расстояния");
             Console.Write("Введите расстояния:");
-            // 1 км стоит 1,5$
-            // f - доллар равен курсу по ЦБ (на сегодняшний день 1$ - 1,7 манат)
-
-            //var f = 2.55;
-
+          
             double usd = 1.7;
             double rate = 1.5;
             double distance = Convert.ToDouble(Console.ReadLine());
